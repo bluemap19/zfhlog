1283a05921abab32541ee5627b8f17a628a185fd
bluemap19 <59407722+bluemap19@users.noreply.github.com>
2025-05-11 15:31:33 +0800

    1

---
+++ src_well_project/LOGGING_PROJECT.py
@@ -0,0 +1,13 @@
+
+
+
+class LOGGING_PROJECT:
+    def __init__(self):
+        PROJECT_PATH=r''
+        WELL_NAMES = ['', '', '']
+        WELL_PATH = {}
+        target_curves = {}
+
+    def init_well_path(self):
+
+        pass
