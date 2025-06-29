75712fb14c1aa40156bcc2f98cfff51a82019a32
bluemap19 <59407722+bluemap19@users.noreply.github.com>
2025-06-29 18:16:31 +0800

    电阻率校正修改

--- 电阻率校正.py
+++ 电阻率校正.py
@@ -12,19 +12,19 @@ from src_plot.plot_scatter_or_line import plot_dataframe
 from src_well_project.LOGGING_PROJECT import LOGGING_PROJECT
 
 
-# 温度影响偏移指数
+# 温度影响偏移 指数函数
 def temp_influence_power_formula(df, A, B):
     return (A*np.power(df['TEMP'], B) - df['R_temp'])/df['R_temp']
     # return (A*np.power(df['TEMP'], B) - df['R_temp'])/(df['R_temp']*df['TEMP'])
     # return (A*np.power(df['TEMP'], B) - df['R_temp'])
 
-# 温度影响偏移指数
+# 温度影响偏移 线性函数
 def temp_influence_linear_formula(df, A, B):
     return (A*df['TEMP'] + B - df['R_temp'])/df['R_temp']
     # return (A*df['TEMP'] + B - df['R_temp'])/(df['R_temp']*df['TEMP'])
     # return (A*np.power(df['TEMP'], B) - df['R_temp'])
 
-
+# 偏移量剔除
 def offset_linear(df):
     fit_result = nonlinear_fitting(df, temp_influence_linear_formula, initial_guess=(200, -5000),
                                    bounds=([50, -np.inf], [350, np.inf]))
@@ -33,6 +33,7 @@ def offset_linear(df):
     print(f"Linear formular success as: A = {A:.4f}, B = {B:.4f}, 残差平方和: {fit_result.cost:.4f}")
     return df
 
+# 偏移量剔除
 def offset_power(df):
     # A:[0.2, 5], B:[1.5, 3]
     fit_result = nonlinear_fitting(df, temp_influence_power_formula, initial_guess=(0.5, 2.5), bounds=([0.2, 1.5], [5, 3]))
@@ -46,9 +47,9 @@ def offset_power(df):
 def fit_r_pred(df, PRED_GAUSS_SETTING={}, offset_function='linear'):
     # 确保输入包含所需列
     if PRED_GAUSS_SETTING:
-        required_cols = ['R_temp', 'TEMP', 'R_pred']
+        required_cols = ['R_temp', 'TEMP', 'R_gauss']
     else:
-        required_cols = ['R_temp', 'TEMP', 'R_real', 'R_pred']
+        required_cols = ['R_temp', 'TEMP', 'R_real', 'R_gauss']
     assert all(col in df.columns for col in required_cols), "DataFrame缺少必要列"
 
     # 执行拟合,先尝试 幂函数R_temp' = A * TEMP^B，再尝试 线性函数 R_temp' = A * TEMP + B
@@ -70,9 +71,9 @@ def fit_r_pred(df, PRED_GAUSS_SETTING={}, offset_function='linear'):
     # plot_dataframe(df, 'R_temp_sub', 'R_real', title=None, X_ticks=None, Y_ticks=None, figure_type='scatter')
 
     if PRED_GAUSS_SETTING:
-        df['R_pred'], stats = scale_gaussian_by_config(source_data=df['R_temp_sub'], target_data_config=PRED_GAUSS_SETTING, return_stats=True)
+        df['R_gauss'], stats = scale_gaussian_by_config(source_data=df['R_temp_sub'], target_data_config=PRED_GAUSS_SETTING, return_stats=True)
     else:
-        df['R_pred'], stats  = scale_gaussian(source_data=df['R_temp_sub'], target_data=df['R_real'], return_stats=True)
+        df['R_gauss'], stats  = scale_gaussian(source_data=df['R_temp_sub'], target_data=df['R_real'], return_stats=True)
     # """"source_mean": μ_source,
     # "source_std": σ_source,
     # "source_range": get_range(source_data),
@@ -115,7 +116,7 @@ def fit_r_pred(df, PRED_GAUSS_SETTING={}, offset_function='linear'):
 #     }
 #     df.rename(columns=column_mapping, inplace=True)
 #     print(df[['DEPTH', 'R_real', 'TEMP', 'R_temp']].describe())
-#     df['R_pred'] = 0
+#     df['R_gauss'] = 0
 #     df['R_temp_sub'] = 0
 #
 #     df = remove_static_depth_data(df)
@@ -163,7 +164,7 @@ if __name__ == '__main__':
     print(LG.WELL_NAMES)
 
     path_logging = LG.search_target_file_path(well_name='02-第9井次  坨73-斜13井',
-                                              target_path_feature=['resampled'],
+                                              target_path_feature=['DATA_ALL'],
                                               target_file_type='logging')
     print(f'current work path :{path_logging}')
 
@@ -175,18 +176,24 @@ if __name__ == '__main__':
 
     # 1. 定义映射关系（原始列名:新列名）
     column_mapping = {
-        'DEPTH':'DEPTH',
+        '#DEPTH':'DEPTH',
         'TEMP':'TEMP',
         'ResFar':'R_temp',
+        'RT_X12':'R_real',
+        # 'RT_X12_LOG':'R_real',
     }
+    column_mapping_inverted = {value: key for key, value in column_mapping.items()}
     df.rename(columns=column_mapping, inplace=True)
     print(df[['DEPTH', 'TEMP', 'R_temp']].describe())
-    df['R_pred'] = 0
     df['R_temp_sub'] = 0
+    df['R_gauss'] = 0
 
+    print(f'df remove depth error before:{df.shape}')
     df = remove_static_depth_data(df)
+    print(f'df remove depth error after:{df.shape}')
 
-    window_work_length = 1000
+
+    window_work_length = df.shape[0]
     windows_step = 200
     windows_num = (df.shape[0] - window_work_length)//windows_step + 2
     windows_view = 1.1
@@ -210,16 +217,16 @@ if __name__ == '__main__':
         # df_window = df.iloc[window_work_start:window_work_end].copy()
         df_window = df.iloc[window_view_start:window_view_end].copy()
 
-        # df_window = fit_r_pred(df_window)
-        df_window = fit_r_pred(df_window, PRED_GAUSS_SETTING={'μ_target': 2.5, 'σ_target': 0.4})
+        df_window = fit_r_pred(df_window, offset_function='power')
+        # df_window = fit_r_pred(df_window, PRED_GAUSS_SETTING={'μ_target': 2.5, 'σ_target': 0.4})
 
         # df.iloc[window_work_start:window_work_end] = df_window
         EFFECTIVE_LENGTH = window_work_end - window_work_start
         df.iloc[window_work_start:window_work_end] = df_window.iloc[window_work_start-window_view_start:window_work_start-window_view_start+EFFECTIVE_LENGTH]
 
-
+    df.rename(columns=column_mapping_inverted, inplace=True)
     time_str = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    df.to_csv(path_logging.replace('02-第9井次  坨73-斜13井--resampled-0.1m_logging', f'坨73-斜13井-{time_str}.csv'), index=False, float_format='%.4f', quoting=csv.QUOTE_NONE)
+    df.to_csv(path_logging.replace('02-第9井次  坨73-斜13井--DATA_ALL_logging.csv', f'坨73-斜13井-{time_str}.csv'), index=False, float_format='%.4f', quoting=csv.QUOTE_NONE)
 
     # # 调用可视化接口
     # visualize_well_logs(
